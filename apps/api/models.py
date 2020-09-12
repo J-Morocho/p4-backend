f5a8d4360f7dad94e4e372fc7a8f55be7fcfa1f9
JDM-B550 <jendri@jdm-b550.home>
2020-09-12 17:36:19 -0400

    add plant and category model

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from apps.authentication.models import  User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Plant(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='plants', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    # No image_url in default plant instance
+    image_url = models.URLField(blank=True, null=True)
+    is_watered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ apps/api/serializers.py
@@ -0,0 +1,2 @@
+from rest_framework import serializers
+from apps.api.models import Category, Plant
