8d6575757c1953be596a530eb59294c221fdbc55
JDM-B550 <jendri@jdm-b550.home>
2020-09-21 22:05:47 -0400

    host

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost:8000', 'localhost:8080']
+ALLOWED_HOSTS = ['localhost:8000', 'localhost:8080', 'https://plantr.netlify.app/#/']
 
 
 # Application definition
