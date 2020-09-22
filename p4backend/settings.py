c1ec020c8bbb1354ef42d24eee3b92bce248e38f
JDM-B550 <jendri@jdm-b550.home>
2020-09-21 21:49:12 -0400

    hide secret key

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'umfn@p&&xk=%d(6qusuxz0@3cnw#9mb#9dlda!gw0m5givfsj5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost:8000']
 
